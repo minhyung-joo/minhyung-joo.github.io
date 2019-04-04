3c4d9800d98e05a789abe2ff9a36e2493a54697e
Minhyung Joo <mjoo@connect.ust.hk>
2019-04-04 17:05:33 +0800

    add responsive meta tag and css baseline

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const Root = () => (
-  <div>
+  <React.Fragment>
+    <CssBaseline />
     <h1>Building in progress!</h1>
     <h2>Built using React.js</h2>
     <Button variant="contained" color="primary">
       Surprise!
     </Button>
-  </div>
+  </React.Fragment>
 );
 
 ReactDOM.render(<Root />, document.getElementById("app"));
