9a482ddf2be6c14b41f9670f1d6e7f9b562e086b
Minhyung Joo <mjoo@connect.ust.hk>
2019-04-04 16:59:18 +0800

    enable material ui

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Button from "@material-ui/core/Button";
 
 const Root = () => (
   <div>
     <h1>Building in progress!</h1>
     <h2>Built using React.js</h2>
+    <Button variant="contained" color="primary">
+      Surprise!
+    </Button>
   </div>
 );
 
