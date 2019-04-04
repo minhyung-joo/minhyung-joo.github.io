ff1c5fad066bef8e6c5d21b3b0ec6ecce222e7d6
Minhyung Joo <mjoo@connect.ust.hk>
2019-04-04 16:41:49 +0800

    set up react

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Root = () => (
+  <div>
+    <h1>Building in progress!</h1>
+    <h2>Built using React.js</h2>
+  </div>
+);
+
+ReactDOM.render(<Root />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true
+  }
+};
